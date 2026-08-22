4d887b37af086d87b0e26940d6ab475e276928c8
shalinirudraswamy-spec <shalinirudraswamy@gmail.com>
2026-08-22 18:53:21 +0530

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,20 @@
+#include "myheader.h"
+
+#define abc 3456
+#define pf printf
+#define coding 65
+
+int main()
+{
+    // delete single line comment
+
+    int k = abc;
+
+    /*
+       remove multi line comment
+    */
+
+    pf("%d %d\n", k, coding);
+
+    return 0;
+}
